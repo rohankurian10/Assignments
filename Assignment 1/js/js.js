1995a0bbb6e5438658be0559fdd1db1c9976062e
Rohan Kurian <rohankurian10@hotmail.com>
2021-08-29 17:51:36 +0530

    Calculator Beta W/O styling

---
+++ Assignment 1/js/js.js
@@ -0,0 +1,16 @@
+txtClear=()=>{
+    document.getElementById("txtVal").value = ""
+}
+
+strVal=(value)=>{
+    document.getElementById("txtVal").value=document.getElementById("txtVal").value+value;
+    console.log(document.getElementById("txtVal").value);
+}
+
+equalsTo=()=>{
+    var txtVal = document.getElementById("txtVal").value
+    console.log(txtVal);
+    var result = eval(txtVal);
+    console.log(result);
+    document.getElementById("txtVal").value = result;
+}
