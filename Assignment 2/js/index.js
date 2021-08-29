0e890cea5878f433c75c513a885dd30d19d89003
Rohan Kurian <rohankurian10@hotmail.com>
2021-08-30 02:49:30 +0530

    Seperate files for js and css

---
+++ Assignment 2/js/index.js
@@ -0,0 +1,10 @@
+var div = document.getElementById("div");
+show = () => {
+    document.getElementById("div").style.display = "block";
+    div.style.display = "block";
+}
+
+hide = () => {
+    document.getElementById("div").style.display = "none";
+    div.style.display = "none";
+}
