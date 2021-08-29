5f3f3569dbb78038b8963cac286cb4bdaf41526e
Rohan Kurian <rohankurian10@hotmail.com>
2021-08-30 02:54:32 +0530

    Changed css and js file locations

---
+++ Assignment 3/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){                           //$(document).ready(function(){});
+    $('#show').click(function(){
+        $('#div').fadeIn(500);
+    });
+    $('#hide').click(function(){
+        $('#div').fadeOut(500);
+    });
+});
